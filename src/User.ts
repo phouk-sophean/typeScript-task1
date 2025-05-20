e166a8c684cbe9e2f5cebfffb9f1772a15ff5a5a
phouk-sophean <phouksophean36@gmail.com>
2025-05-20 15:34:17 +0700

    creating structure and user file were done

---
+++ src/User.ts
@@ -0,0 +1,30 @@
+class User {
+    private id: number;
+    private name: string;
+    private email: string;
+    private password: string;
+
+    constructor(id: number, name: string, email: string, password: string){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public logic(): boolean {
+        return true
+    }
+    public register(): void {
+        
+    }
+    public updatePassword(newPassword: string): void {
+        this.password = newPassword;
+    }
+    public updageProfile(newName: string, newEmail: string): void{
+        this.name = newName;
+        this.email = newEmail;
+    }
+    public getName() {
+        return this.name;
+    }
+}
